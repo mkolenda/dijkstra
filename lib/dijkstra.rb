a674a966e7d22fdee7679e61207f84eb3fcdb2e2
Matt Kolenda <mkolenda@gmail.com>
2016-03-20 21:27:52 -0400

    graph is now an instance

--- lib/dijkstra.rb
+++ lib/dijkstra.rb
@@ -2,40 +2,18 @@ require_relative "dijkstra/version"
 
 module Dijkstra
   class Compute
-    attr_accessor :destination, :best_journey
+    attr_accessor :destination, :best_journey, :graph
 
     def initialize(start:, destination:, file:)
-      reset
-      GraphBuilder.build_graph(file)
-      check_params(start, destination)
+      self.graph = Graph.new(file)
       self.destination = destination
+
+      check_params(start, destination)
       i_walk_the_line(Journey.new(path: [start], cost: 0))
       report_winner
     end
 
     private
-
-    def save_winner(journey)
-      self.best_journey = journey
-    end
-
-    def report_winner
-      if best_journey
-        puts("Shortest path is [#{best_journey.path.join(',')}] with total cost #{best_journey.cost}")
-      else
-        puts("No winner found")
-      end
-    end
-
-    def reset
-      Node.reset
-      self.best_journey = nil
-    end
-
-    def check_params(start, destination)
-      raise(ArgumentError, "The start node #{start} is invalid. Should be one of #{Node.ids}. Exiting") unless Node.find(start)
-      raise(ArgumentError, "The destination node #{destination} is invalid. Should be one of #{Node.ids}. Exiting") unless Node.find(destination)
-    end
 
     def i_walk_the_line(journey)
       return if journey.cycle?
@@ -46,33 +24,76 @@ module Dijkstra
         return
       end
 
-      Node.find(journey.path.last).connections.each do |connection|
+      graph.find(journey.path.last).connections.each do |connection|
         next_node, connection_cost = *connection
         new_cost = journey.cost + connection_cost
         i_walk_the_line(journey.take_a_step(node: next_node, cost: new_cost))
       end
     end
+
+    def save_winner(journey)
+      self.best_journey = journey
+    end
+
+    def report_winner
+       puts(best_journey ? "Shortest path is [#{best_journey.path.join(',')}] with total cost #{best_journey.cost}" : "No winner found")
+    end
+
+    def check_params(start, destination)
+      raise(ArgumentError, "The start node #{start} is invalid. Should be one of #{graph.ids}. Exiting") unless graph.find(start)
+      raise(ArgumentError, "The destination node #{destination} is invalid. Should be one of #{graph.ids}. Exiting") unless graph.find(destination)
+    end
+
   end
 
-  class GraphBuilder
-    class << self
-      def build_graph(file)
-        raise(ArgumentError, "Could not locate graph file #{file}.  Exiting.") unless File.exists?(file)
+  class Graph
+    attr_accessor :nodes
 
-        File.foreach(file) do |record|
-          clean_node_record!(record)
-          next if record.empty?
-          from, to, cost = record.split(',')
-          from = Node.find_or_create(from)
-          to = Node.find_or_create(to)
-          Node.add_connection(from: from, to: to, cost: cost.to_i)
-        end
-        nil
-      end
+    def initialize(file)
+      raise(ArgumentError, "Could not locate graph file #{file}.  Exiting.") unless File.exists?(file)
+      self.nodes = Hash.new
 
-      def clean_node_record!(record)
-        record.gsub!(/[\[\]\s]/, '')
+      File.foreach(file) do |record|
+        clean_node_record!(record)
+        next if record.empty?
+        from, to, cost = record.split(',')
+        from = find_or_create(from)
+        to   = find_or_create(to)
+        add_connection(from: from, to: to, cost: cost.to_i)
       end
+    end
+
+    def clean_node_record!(record)
+      record.gsub!(/[\[\]\s]/, '')
+    end
+
+    def add_node(node)
+      nodes[node.id] = node
+    end
+
+    def add_connection(from:, to:, cost:)
+      from.connections << [to.id, cost]
+    end
+
+    def find_or_create(id)
+      find(id) || add_node(Node.new(id))
+    end
+
+    def find(id)
+      nodes[id]
+    end
+
+    def ids
+      nodes.keys
+    end
+  end
+
+  class Node
+    attr_accessor :connections, :id
+
+    def initialize(id)
+      @id = id
+      @connections = []
     end
   end
 
@@ -107,52 +128,6 @@ module Dijkstra
 
     def loser?(other_journey)
       !winner?(other_journey)
-    end
-  end
-
-  class Node
-    attr_accessor :connections, :id
-    @nodes = Hash.new
-
-    def initialize(id)
-      @id = id
-      @connections = []
-      self.class.add_node(self)
-    end
-
-    class << self
-      attr_accessor :nodes
-
-      def add_node(node)
-        nodes[node.id] = node
-      end
-
-      def add_connection(from:, to:, cost:)
-        from.connections << [to.id, cost]
-      end
-
-      def find_or_create(id)
-        find(id) || new(id)
-      end
-
-      def find(id)
-        nodes[id]
-      end
-
-      def ids
-        nodes.keys
-      end
-
-      def all
-        nodes.each_pair do |id, node|
-          puts "node: #{id} connections #{node.connections}"
-        end
-        nil
-      end
-
-      def reset
-        @nodes = Hash.new
-      end
     end
   end
 end
